e678f9f5b468f4aaa9564fe73c43420633257db1
Brock Bloomquist <brockbloomquist1@gmail.com>
2024-06-01 11:36:20 -0500

    Added packages for bootstrap and react bootstrap and created first page of list of users

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { SmallExample } from "./components/SmallExample";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 class="d-flex justify-content-center mb-3">Users</h1>
+      <SmallExample />
+    </>
   );
 }
 
